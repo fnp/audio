dcf7736e1811536ed142bb2de32288faf22f2f0e
Radek Czajka <radoslaw.czajka@nowoczesnapolska.org.pl>
2011-06-14 16:17:40 +0200

    fix

--- apps/archive/forms.py
+++ apps/archive/forms.py
@@ -23,10 +23,10 @@ class AudiobookForm(forms.ModelForm):
             if not os.path.isdir(FILES_PATH):
                 os.makedirs(FILES_PATH)
             # save the file in model
-            bn = os.path.basename(path)
-            ef = ExistingFile(path)
-            
-            m.source_file.save(bn, ef)
+
+            m.source_file.save(
+                os.path.join(FILES_PATH, os.path.basename(path)),
+                ExistingFile(path))
 
         if commit:
             m.save()
