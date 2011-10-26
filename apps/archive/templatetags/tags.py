e927d217b3ad62c17b43c34ca5838c61fc1928e8
Radek Czajka <radoslaw.czajka@nowoczesnapolska.org.pl>
2011-10-26 12:52:26 +0200

    fix

--- apps/archive/templatetags/tags.py
+++ apps/archive/templatetags/tags.py
@@ -5,11 +5,12 @@ register = template.Library()
 @register.inclusion_tag('archive/tags/multiple_tags_table.html')
 def multiple_tags_table(tags, table=True):
     new_tags = {}
-    for k, v in tags.items():
-        if isinstance(v, list):
-            new_tags[k] = v
-        else:
-            new_tags[k] = [v]
+    if tags:
+        for k, v in tags.items():
+            if isinstance(v, list):
+                new_tags[k] = v
+            else:
+                new_tags[k] = [v]
     return {"tags": new_tags, "table": table}
 
 
