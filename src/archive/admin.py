3bbec5643861e1efd4364bdbf593d705facc6682
Radek Czajka <rczajka@rczajka.pl>
2020-06-05 15:19:32 +0200

    fix

--- src/archive/admin.py
+++ src/archive/admin.py
@@ -7,7 +7,7 @@ admin.site.register(Project)
 class AudiobookAdmin(admin.ModelAdmin):
     list_display = ["title", "slug", "index", "part_name", "license", "youtube_volume"]
     list_filter = ["license"]
-    list_search = ["title", "slug", "part_name", "youtube_volume"]
+    search_fields = ["title", "slug", "part_name", "youtube_volume"]
     list_editable = ["youtube_volume"]
 
 
